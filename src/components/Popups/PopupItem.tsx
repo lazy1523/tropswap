c0b48ea31967efb197589d2783d4d89ac1a44956
紫苏 <lazy1523@outlook.com>
2022-01-26 10:34:24 +0800

    fix swap

--- src/components/Popups/PopupItem.tsx
+++ src/components/Popups/PopupItem.tsx
@@ -76,9 +76,9 @@ export default function PopupItem({
     popupContent = <TransactionPopup hash={hash} success={success} summary={summary} />
   } else if ('listUpdate' in content) {
     const {
-      listUpdate: { listUrl, oldList, newList, auto }
+      listUpdate: { listUrl, oldList, newList }
     } = content
-    popupContent = <ListUpdatePopup popKey={popKey} listUrl={listUrl} oldList={oldList} newList={newList} auto={auto} />
+    popupContent = <ListUpdatePopup popKey={popKey} listUrl={listUrl} oldList={oldList} newList={newList} auto={false} />
   }
 
   const faderStyle = useSpring({
